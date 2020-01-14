4de4ec5f2dc385f222184b116e5b7c42c3030e28
Marcin Kliks <marcin.kliks@allegro.pl>
2020-01-14 13:35:19 +0100

    Bump swift 5.1.3

--- Package.swift
+++ Package.swift
@@ -14,9 +14,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "calendarsync",
-            dependencies: []),
-        .testTarget(
-            name: "calendarsyncTests",
-            dependencies: ["calendarsync"]),
+            dependencies: [])
     ]
 )
